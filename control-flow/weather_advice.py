014fadde408aa1d8701551c9ee6e70d090d61428
sandysameh2 <sandy.sameh@nodogoro.com>
2024-06-30 16:35:18 +0300

    weather flow

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
+
+match weather: 
+    case "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    case "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+    case "cold":
+        print("Make sure to wear a warm coat and a scarf.")
+    case _:
+        print("Sorry, I don't have recommendations for this weather.")
