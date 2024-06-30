30bcd0b5fc07a4c5283d5f92791f682c342e9d88
sandysameh2 <sandy.sameh@nodogoro.com>
2024-06-30 16:36:57 +0300

    weather flow 2

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,20 @@
 weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 
-match weather: 
-    case "sunny":
-        print("Wear a t-shirt and sunglasses.")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "cold":
-        print("Make sure to wear a warm coat and a scarf.")
-    case _:
-        print("Sorry, I don't have recommendations for this weather.")
+if weather =="sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else: 
+    print("Sorry, I don't have recommendations for this weather.")
+
+# match weather: 
+#     case "sunny":
+#         print("Wear a t-shirt and sunglasses.")
+#     case "rainy":
+#         print("Don't forget your umbrella and a raincoat.")
+#     case "cold":
+#         print("Make sure to wear a warm coat and a scarf.")
+#     case _:
+#         print("Sorry, I don't have recommendations for this weather.")
