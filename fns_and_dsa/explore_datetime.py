0637eb8633c5205bd83fb732e256756503dd16ae
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-06 22:45:57 +0300

    checks for number

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from datetime import timedelta
+
+def display_current_datetime():
+    current_date=datetime.now()
+    print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
+    return current_date
+
+def calculate_future_date():
+    days = input("Enter the number of days to add to the current date:")
+    if days.isdigit(): 
+        days = int(days)
+        delta = display_current_datetime() +timedelta(days=days)
+        
+        print(f"Future date:{delta.strftime('%Y-%m-%d')}")
+        
+    else :
+        print("Please enter a valid number")
+calculate_future_date()
