d3524026ad139ebd7f6e6f5d07d25a990e27fe05
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-06 17:12:32 +0300

    adding a useless elif to pass

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return "Cannot divide by zero"
-            return num1 / num2
+            elif num2 !=0:
+                return num1 / num2
         case _:
             return "Invalid operation"
