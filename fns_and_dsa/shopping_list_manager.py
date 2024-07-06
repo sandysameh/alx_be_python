bb941e7b6e1ac94481113e41cc6feccbe1e5e814
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-06 22:21:17 +0300

    shopping manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,39 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            addedItem =input("Item you want to add ?")
+            shopping_list.append(addedItem)
+            # Prompt for and add an item
+            pass
+        elif choice == '2':
+            removedItem =input("Item you want to remove ?")
+            if removedItem in shopping_list:
+                shopping_list.remove(removedItem)
+            else: 
+                print("Item doesn't exist in cart")
+            # Prompt for and remove an item
+            pass
+        elif choice == '3':
+            for item in shopping_list:
+                print(item +" ,", end="")
+            # Display the shopping list
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
