26e2f3a546d707eb9069636796416d5b2ccc9d3f
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-06 17:10:39 +0300

    perform operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,10 +1,14 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
             return num1 + num2
         case 'subtract':
-            return num1 -num2 
+            return num1 - num2
         case 'multiply':
             return num1 * num2
         case 'divide':
-            return num1 / num2 
+            if num2 == 0:
+                return "Cannot divide by zero"
+            return num1 / num2
+        case _:
+            return "Invalid operation"
