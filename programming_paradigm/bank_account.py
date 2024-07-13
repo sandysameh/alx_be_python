06fff325c02c7125da129ddbade9d90867ef1c84
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-14 00:26:23 +0300

    bank acounts

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,5 +14,5 @@ class BankAccount:
             return True
         
     def display_balance(self):
-        print(f"Your Current Balance:{self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
