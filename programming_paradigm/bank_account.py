5134532a2ad802862a0156fb7dd86aff4f9219ac
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-14 00:22:04 +0300

    bank acounts

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount: 
+    def __init__(self,account_balance=0.0):
+        self.account_balance = account_balance
+    
+    def deposit(self,amount):
+        self.account_balance+=amount
+        return self.account_balance
+
+    def withdraw(self,amount):
+        if amount > self.account_balance:
+            return False
+        else : 
+            self.account_balance -= amount
+            return True
+        
+    def display_balance(self):
+        print(f"Your current balance is {self.account_balance}")
+
