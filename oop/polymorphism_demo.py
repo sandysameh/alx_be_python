c593ea11597ec789b7584be9db5b147b27a8cdab
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-28 17:41:38 +0300

    polymorephism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -15,4 +15,4 @@ class Circle(Shape):
     def __init__(self,radius) :
         self.radius= radius
     def area(self):
-        return self.radius * self.radius * math.pi
+        return (self.radius **2) * math.pi
