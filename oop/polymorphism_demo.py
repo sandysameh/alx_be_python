1cdae1ebdd1deb7fdd297ef45b789c3e125157f7
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-28 17:40:29 +0300

    polymorephism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+import math
+
+class Shape:
+    def area():
+        raise NotImplementedError
+    
+class Rectange(Shape):
+    def __init__(self,length,width) :
+        self.length= length
+        self.width = width 
+    def area(self):
+        return self.width * self.length
+
+class Circle(Shape):
+    def __init__(self,radius) :
+        self.radius= radius
+    def area(self):
+        return self.radius * self.radius * math.pi
