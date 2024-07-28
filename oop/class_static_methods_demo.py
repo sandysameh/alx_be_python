848dfd41651a00542eef7730f6fc2667f73033e9
sandysameh2 <sandy.sameh@nodogoro.com>
2024-07-28 17:54:48 +0300

    polymorephism

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,7 +2,7 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
     @staticmethod
     def add(a,b):
-        return a+b
+        return a + b
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
